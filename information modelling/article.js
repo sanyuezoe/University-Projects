d667138b1fa47275d11bfe0f466d4f8cb39a20cb
Zoe Zhao <92298701+sanyuezoe@users.noreply.github.com>
2023-08-07 13:40:52 +0200

    Add files via upload

---
+++ information modelling/article.js
@@ -0,0 +1,614 @@
+const Penthesilea = [
+  {
+      name: "Penthesilea (22)",
+      type: "people",
+      link:"https://en.wikipedia.org/wiki/Penthesilea"
+  },
+  {
+      name: "Hippolyta (9)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Hippolyta"
+  },
+  {
+      name: "Priam (4)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Priam"
+  },
+  {
+      name: "Achilles (15)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Achilles"
+  },
+  {
+      name: "Patroclus (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Patroclus"
+  },
+  {
+      name: "Hector (10)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Hector"
+  },
+  {
+      name: "Paris (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Paris_(mythology)"
+  },
+  {
+      name: "Glaucus (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Glaucus"
+  },
+  {
+      name: "Aeneas (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Aeneas"
+  },
+  {
+    name: "Zeus (1)",
+    type: "people" ,
+    link:"https://en.wikipedia.org/wiki/Zeus"
+},
+{
+  name: "Hermes (1)",
+  type: "people" ,
+  link:"https://en.wikipedia.org/wiki/Hermes"
+},    
+  {
+      name: "Amazon (5)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Amazons"
+  },
+  {
+      name: "Troy (5)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Troy"
+  },
+  {
+      name: "Scamandrian plains (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Scamandria"
+  },
+  {
+      name: "Scythian (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Scythian"
+  },
+  {
+      name: "Simois (2)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Simoeis"
+  },
+  {
+      name: "Phrygia (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Phrygia"
+  },
+  {
+    name:"death (2)",
+    type:"concept",
+    link:"#"
+  },
+  {
+    name:"revenge (1)",
+    type:"concept",
+    link:"#"
+  },
+
+]
+
+const Hecabe = [
+  {
+      name: "Cassandra (23)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Cassandra"
+  },
+  {
+      name: "Odysseus (39)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Odysseus"
+  },
+  {
+      name: "Hecabe (21)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Hecuba"
+  },
+  {
+      name: "Andromache (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Andromache"
+  },
+  {
+      name: "Polyxena (2)",
+      type: "people",
+      link:"https://en.wikipedia.org/wiki/Polyxena"
+  },
+  {
+      name: "Polymestor (34)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Polymestor"
+  },
+  {
+      name: "Priam (6)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Priam"
+  },
+  {
+      name: "Agamemnon (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Agamemnon"
+  },
+  {
+      name: "Hector (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Hector"
+  },
+  {
+      name: "Paris (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Paris_(mythology)"
+  },
+  {
+      name: "Achilles (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Achilles"
+  },
+  {
+      name: "Thrace (4)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Thrace"
+  },
+  {
+      name: "Troy (11)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Troy"
+  },
+  {
+      name: "Chersonese (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Chersonesus"
+  },
+  {
+      name: "Ithaca (3)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Homer%27s_Ithaca"
+  },
+  {
+    name:"death (1)",
+    type:"concept",
+    link:"#"
+  },
+  {
+    name:"revenge (2)",
+    type:"concept",
+    link:"#"
+  },
+
+
+]
+
+const Medea = [
+  {
+      name: "Medea (68)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Medea"
+  },
+  {
+      name: "Euripides (9)",
+      type: "people",
+      link:"https://en.wikipedia.org/wiki/Euripides" 
+  },
+  {
+      name: "Aigeus (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Aegeus"
+  },
+  {
+      name: "Crayon (5)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Creon_(king_of_Thebes)"
+  },
+  {
+      name: "Jason (20)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Jason"
+  },
+  {
+      name: "Agameda (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Agamede"
+  },
+  {
+      name: "Akamas (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Acamas"
+  },
+  {
+      name: "Glauke (3)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Glauce"
+  },
+  {
+      name: "Leukon (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Leucon_I"
+  },
+  {
+      name: "Aeetes (3)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Ae%C3%ABtes"
+  },
+  {
+      name: "Apsyrtus (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Absyrtus"
+  },
+  {
+    name: "Achilles (1)",
+    type: "people" ,
+    link:"https://en.wikipedia.org/wiki/Achilles"
+},
+  {
+      name: "Creon (4)",
+      type: "people",
+      link:"https://en.wikipedia.org/wiki/Creon_(king_of_Corinth)" 
+  },
+  {
+      name: "Athens (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Athens#History"
+  },
+
+  {
+      name: "Colchis (6)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Colchis"
+  },
+  {
+      name: "Corinth (10)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Ancient_Corinth"
+  },
+
+]
+
+const Circe = [
+  {
+      name: "Aeetes (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Ae%C3%ABtes"
+  },
+  {
+      name: "Zeus (7) ",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Zeus"
+  },
+  {
+      name: "Athena (7)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Athena"
+  },
+  {
+      name: "Odysseus (12)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Odysseus"
+  },
+  {
+      name: "Penelope (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Penelope"
+  },
+  {
+      name: "Eileithya (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Ethausva"
+  },
+  {
+      name: "Telegonus (10)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Telegonus_(son_of_Odysseus)"
+  },
+  {
+      name: "Astyanax (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Astyanax"
+  },
+  {
+      name: "Hermes (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Hermes"
+  },    
+  {
+      name: "Apollo (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Apollo"
+  },
+  {
+      name: "polymetis (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Perittia"
+  },
+  {
+    name: "Daedalus (1)",
+    type: "people" ,
+    link:"https://en.wikipedia.org/wiki/Daedalus"
+},
+  {
+      name: "Oceanos' halls (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Oceanus"
+  },
+  {
+      name: "Troy (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Troy"
+  },
+  {
+      name: "Olympus (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Mount_Olympus"
+  }
+]
+
+const Klytemnestra = [
+  {
+      name: "Klytemnestra (7)",
+      type: "people",
+      link:"https://en.wikipedia.org/wiki/Clytemnestra" 
+  },
+  {
+      name: "Agamemnon (36)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Agamemnon"
+  },
+  {
+      name: "Iphigenia (4)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Iphigenia"
+  },
+  {
+      name: "Atrides/Atreo (4)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Atreus#Atreides"
+  },
+  {
+      name: "Paris (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Paris_(mythology)"
+  },
+  {
+      name: "Cassandra (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Cassandra"
+  },
+  {
+      name: "Electra (3)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Electra"
+  },
+  {
+    name: "Zeus (2) ",
+    type: "people" ,
+    link:"https://en.wikipedia.org/wiki/Zeus"
+},
+{
+  name: "Athena (1)",
+  type: "people" ,
+  link:"https://en.wikipedia.org/wiki/Athena"
+},
+
+  {
+    name: "Euripides (1)",
+    type: "people" ,
+    link:"https://en.wikipedia.org/wiki/Euripides" 
+},
+  {
+      name: "Oreste (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Orestes"
+  },
+  {
+      name: "Mycene (12)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Mycenae"
+  },
+  {
+      name: "Troy (1)",
+      type: "place" ,
+      link:"https://en.wikipedia.org/wiki/Troy"
+  }
+
+]
+
+const Girlsmeetsboys = [
+  {
+      name: "Iphis/Robin (52)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Iphis"
+  },
+  {
+      name: "Ovid (36)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Ovid"
+  },
+  {
+      name: "Anthea (20)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Hera"
+  },
+  {
+      name: "Imogen (15)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Imogen_(given_name)"
+  },
+  {
+      name: "Pasiphae (3)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Pasipha%C3%AB"
+  },
+  {
+      name: "Achilles (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Achilles"
+  },
+  {
+      name: "Agamemnon (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Agamemnon"
+  },
+  {
+      name: "Mars (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Mars_(mythology)"
+  },
+  {
+      name: "Cassandra (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Cassandra"
+  },
+  {
+      name: "Venus (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Venus_(mythology)"
+  },
+  {
+      name: "Daedalus (2)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Daedalus"
+  },
+  {
+      name: "isis (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Isis"
+  },
+  {
+      name: "Sappho (1)",
+      type: "people" ,
+      link:"https://en.wikipedia.org/wiki/Sappho"
+  },
+]
+const iframe = document.getElementById('article-frame');
+var options = document.querySelectorAll('input[name="highlight-option"]');
+const buttons = document.querySelectorAll('.issue-button button');
+
+function loadArticle(articleFile) {
+  uncheckAllCheckboxes();
+  iframe.src = articleFile;
+}
+
+
+function uncheckAllCheckboxes() {
+  options.forEach((option) => {
+    option.checked = false;
+  });
+}
+
+
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
+    const articleFile = this.getAttribute('data-article');
+    loadArticle(articleFile);
+
+    const arrayMap = {
+      'Penthesilea': Penthesilea,
+      'Hecabe': Hecabe,
+      'Medea': Medea,
+      'Circe':Circe,
+      'Klytemnestra':Klytemnestra,
+      'Girls Meets Boys':Girlsmeetsboys,
+      // Add more entries for other buttons and arrays
+    };
+
+    const clickedArray = arrayMap[this.textContent] || [];
+    populateMetadataLists(clickedArray);
+  });
+});
+
+function populateMetadataLists(array) {
+  const peopleList = document.getElementById('people-list');
+  const placeList = document.getElementById('place-list');
+  const conceptList = document.getElementById('concept-list');
+
+  function createLinkElement(name, link) {
+    const linkElement = document.createElement('a');
+    linkElement.textContent = name;
+    linkElement.href = link; 
+    linkElement.target = '_blank'; // Set the target attribute to open the link in a new tab (optional)
+
+    return linkElement;
+  }
+
+  peopleList.innerHTML = '';
+  placeList.innerHTML = '';
+  conceptList.innerHTML = '';
+
+  array.forEach((item) => {
+    const linkElement = createLinkElement(item.name, item.link); 
+
+    const linkWrapper = document.createElement('div');
+    linkWrapper.appendChild(linkElement);
+
+
+    if (item.type === 'people') {
+        peopleList.appendChild(linkWrapper);
+      } else if (item.type === 'place') {
+        placeList.appendChild(linkWrapper);
+      } else if (item.type === 'concept') {
+        conceptList.appendChild(linkWrapper);
+      }
+  });
+}
+
+options.forEach(function (option) {
+  option.addEventListener('change', function () {
+    var selectedOptions = getSelectedOptions();
+    highlightText(selectedOptions);
+  });
+});
+
+
+function getSelectedOptions() {
+  var selectedOptions = [];
+  options.forEach(function (option) {
+    if (option.checked) {
+      selectedOptions.push(option.value);
+    }
+  });
+  return selectedOptions;
+}
+
+function highlightText(selectedOptions) {
+  var message = {
+    type: 'highlight',
+    selectedOptions: selectedOptions,
+    cssClassPrefix: 'highlight-' // Prefix for CSS class names
+  };
+
+  iframe.contentWindow.postMessage(message, '*');
+}
+
+window.addEventListener('message', function (event) {
+  var message = event.data;
+
+  if (message.type === 'highlight') {
+    var selectedOptions = message.selectedOptions;
+    var cssClassPrefix = message.cssClassPrefix;
+
+    // Remove all previous highlight CSS classes
+    var elements = document.querySelectorAll('.highlight-name, .highlight-place, .highlight-concept');
+    elements.forEach(function (element) {
+      element.classList.remove('highlight-name', 'highlight-place', 'highlight-concept');
+    });
+
+    // Add highlight CSS classes for selected options
+    selectedOptions.forEach(function (option) {
+      var cssClass = cssClassPrefix + option;
+      var elements = document.getElementsByClassName(option);
+
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add(cssClass);
+      }
+    });
+  }
+});
+
---
+++ information modelling/script.js
@@ -0,0 +1,67 @@
+
+  var map = L.map('map').setView([41.961462340681386, 25.240441126302898], 5);
+
+  var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+osm.addTo(map);
+
+
+const OlympusIcon = L.icon({
+  iconUrl:"/img/olympus-icon.png",
+  iconSize:[15, 15],
+  iconAnchor: [10, 10] ,
+})
+
+const PlainIcon = L.icon({
+  iconUrl:"/img/plain-icon.png",
+  iconSize:[15, 15],
+  iconAnchor: [10, 10] ,
+})
+
+const RiverIcon = L.icon({
+  iconUrl:"/img/river-icon.png",
+  iconSize:[15, 15],
+  iconAnchor: [10, 10] ,
+})
+
+const CityIcon = L.icon({
+  iconUrl:"/img/city-icon.png",
+  iconSize:[15, 15],
+  iconAnchor: [10, 10] ,
+})
+
+const IslandIcon = L.icon({
+  iconUrl:"/img/island-icon.png",
+  iconSize:[15, 15],
+  iconAnchor: [10, 10] ,
+})
+
+
+
+// Add markers directly to the map
+var marker1 = L.marker([39.961462340681386, 26.240441126302898], { icon: CityIcon }).addTo(map);
+var marker2 = L.marker([39.558413985710835, 26.569490132413435], { icon: PlainIcon  }).addTo(map); //Scamandrian plains
+var marker3 = L.marker([39.992722, 26.208647], { icon: RiverIcon }).addTo(map); //Simois river
+var marker4 = L.marker([39.650356, 31.978243], { icon: CityIcon }).addTo(map); // Phrygia capital
+var marker5 = L.marker([45.300181089333854, 34.39997853899846], { icon: CityIcon }).addTo(map); // Chersonese
+var marker6 = L.marker([38.366667, 20.716667], { icon: IslandIcon }).addTo(map); // Ithaca island
+var marker7 = L.marker([40.085556, 22.358611], { icon: OlympusIcon }).addTo(map); // Olympus mountains
+var marker8 = L.marker([37.730278, 22.7575], { icon: CityIcon }).addTo(map); // Mycene
+var marker9 = L.marker([37.983800, 23.727500], { icon: CityIcon }).addTo(map); // ancient Athens
+var marker10 = L.marker([42.25, 42.7], { icon: CityIcon }).addTo(map); // Colchis
+var marker11 = L.marker([37.938611, 22.927222], { icon: CityIcon }).addTo(map); // Corinth
+
+// Bind popups to markers
+marker1.bindPopup( "<a href='https://en.wikipedia.org/wiki/Troy'>Ancient City of Troy</a>");
+marker2.bindPopup("Scamandrian plain");
+marker3.bindPopup("<a href='https://en.wikipedia.org/wiki/Simoeis'>Simois River</a>");
+marker4.bindPopup("<a href='https://en.wikipedia.org/wiki/Phrygia'>Phrygia</a>");
+marker5.bindPopup("<a href='https://en.wikipedia.org/wiki/Chersonesus'>Chersonese</a>");
+marker6.bindPopup("<a href='https://en.wikipedia.org/wiki/Homer%27s_Ithaca'>Ithaca island</a>");
+marker7.bindPopup("<a href='https://en.wikipedia.org/wiki/Mount_Olympus'>Olympus mountains</a>");
+marker8.bindPopup("<a href='https://en.wikipedia.org/wiki/Mycenae'>Mycene</a>");
+marker9.bindPopup("<a href='https://en.wikipedia.org/wiki/History_of_Athens#Antiquity'>ancient Athens</a>");
+marker10.bindPopup("<a href='https://en.wikipedia.org/wiki/Colchis'>Colchis</a>");
+marker11.bindPopup("<a href='https://en.wikipedia.org/wiki/Colchis'>Corinth</a>");
